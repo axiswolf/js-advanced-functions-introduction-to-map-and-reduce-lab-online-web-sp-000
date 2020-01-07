1976ad2755ec403fb24020112ee1c24fe1b88660
Karen V <karenvo@usc.edu>
2020-01-07 13:01:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Your code here
-functuon mapToNegativize(array) {
-  return array.map(x => x * -1)
+function mapToNegativize(array) {
+  return array.map(i => i * -1)
 }
 
 function mapToNoChange() {
