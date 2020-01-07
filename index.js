a8098d89e611e6a52a29ea654ad99eb458d42f74
Karen V <karenvo@usc.edu>
2020-01-07 12:45:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function mapToNegativize(array) {
+  return array * -1
+}
+
+function mapToNoChange() {
+  
+}
+
+function mapToDouble() {
+  
+}
+
+function mapToSquare() {
+  
+}
